3d155080ab88d7afc708eeba5990f7773e88761f
Bratislav <37560533+BratislavStojanovic@users.noreply.github.com>
2019-02-03 14:04:05 +0100

    Add files via upload

---
+++ app/config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+// DB PARAMS
+define('DB_HOST','localhost');
+define('DB_USER','_YOUR_USER_');
+define('DB_PASS','_YOUR_PSSS_');
+define('DB_NAME','_YOUR_DBNAME_');
+
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL ROOT
+define('URLROOT', '_YOUR_URL_');
+
+//Site Name
+define('SITENAME','_YOUR_SITENAME_');
